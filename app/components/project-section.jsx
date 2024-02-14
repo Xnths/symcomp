ca4f5b98e59c162a3a79bf0b1bfd0b9311dbcebf
yesmanic <yesman.choque@gmail.com>
2024-02-13 22:24:21 -0300

    feat: add basic carousel component

--- app/components/project-section.jsx
+++ app/components/project-section.jsx
@@ -9,16 +9,16 @@ export default function ProjectSection() {
 
 
             <div className="grid grid-flow-col auto-cols-max overflow-x-auto gap-10 p-5 w-full max-w-[1200px] justify-between">
-                <ProjectCard buttonText="Semana da Computação" srcImage="/semana-logo.png">
+                <ProjectCard buttonText="Semana da Computação" srcImage="/logos/semana-logo.png">
                     Lorem Ipsum is simply dummy text of the the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
                 </ProjectCard>
                 
                 
-                <ProjectCard buttonText="ByteCafé" srcImage="/bytecafe-logo.png">
+                <ProjectCard buttonText="ByteCafé" srcImage="/logos/bytecafe-logo.png">
                     Lorem Ipsum is simply dummy text of the the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
                 </ProjectCard>
 
-                <ProjectCard buttonText="ByteCafé" srcImage="/bytecafe-logo.png">
+                <ProjectCard buttonText="ByteCafé" srcImage="/logos/bytecafe-logo.png">
                     Lorem Ipsum is simply dummy text of the the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
                 </ProjectCard>
             </div>
