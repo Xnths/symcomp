c69e0bfc514946de63855f79aba566640865075d
Od4ir <od4ircode@gmail.com>
2024-02-21 20:40:19 -0300

    Info's layout on presentation done

--- app/atomic/item-event-info.jsx
+++ app/atomic/item-event-info.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import "../styles/semana.css";
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 
 export default function ItemEventInfo (props) {
     return (
         <div className='presentation__eventinfos'>
-            <span className='presentation__eventinfos_date'>22 A 26 DE ABRIL</span>
-            <span className='presentation__eventinfos_location'>
-                <p id="presentation__eventinfos_local_name">AUDITÓRIO JACY MONTEIRO </p>
-                <p id="presentation__eventinfos_local_adress">R. do Matão, 1010 - BUtantã, São Paulo - SP, 05508-090</p>
-            </span>
+            <div className='presentation__eventinfos_date'>
+                <CalendarMonthIcon id="presentation__eventinfos_calendaricon" className='presentation__icons'/>
+
+                <span id="presentation__eventinfos_datenumber">22 A 26 DE ABRIL</span>
+            </div>
+            <div className='presentation__eventinfos_location'>
+                <LocationOnIcon id="presentation__eventinfos_locationicon" className='presentation__icons'/>
+                <div className='presentation__eventinfos_text'>
+                    <span id="presentation__eventinfos_local_name">AUDITÓRIO JACY MONTEIRO </span>
+                    <span id="presentation__eventinfos_local_adress">R. do Matão, 1010 - BUtantã, São Paulo - SP, 05508-090</span>
+                </div>
+            </div>
         </div>
     )
 }
