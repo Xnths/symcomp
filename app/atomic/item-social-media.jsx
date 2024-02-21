9419be092160b113339903ba1a2360fa5d574b0d
Xnths <jonathascastilhoes@gmail.com>
2024-02-21 17:45:44 -0300

    feat: Adding social media icons

--- app/atomic/item-social-media.jsx
+++ app/atomic/item-social-media.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Image from "next/image";
+
 import "../styles/semana.css";
 
 export default function ItemSocialMedia(props) {
@@ -7,9 +9,14 @@ export default function ItemSocialMedia(props) {
             <p>REDES SOCIAIS</p>
             <div className="presentation__socialmedia_icons">
                 <div id="instagram">
+                    <Image src="/icons/icon_instagram.svg" width={48} height={48} />
                 </div>
-                <div id="email"></div>
-                <div id="linkedin"></div>
+                <div id="email">
+                    <Image src="/icons/icon_email.svg" width={48} height={48} />
+                </div>
+                <div id="linkedin">
+                    <Image src="/icons/icon_linkedin.svg" width={48} height={48} />
+                </div>
             </div>
         </div>
     )
