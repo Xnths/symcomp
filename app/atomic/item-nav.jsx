46b9eba59aa885b9b4138af24f2d42d28dbb7a88
Xnths <jonathascastilhoes@gmail.com>
2024-02-21 15:29:28 -0300

    feat: Implementing the nav menu

--- app/atomic/item-nav.jsx
+++ app/atomic/item-nav.jsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import "../styles/semana.css"
 
 /**
  * Component to display a navigation item given a section it should direct the user to.
  * @param {Object} props - The component's properties.
+ * @param {boolean} props.selected - Attribute true if it is the selected nav Item or false otherwise.
+ * @param {funciton} props.onClick - Function that manages the clicks.
  * @param {string} props.navigateTo - Name of the section the item should direct the user to.
  * @param {string} props.innerText - Name that will appear inside the item.
  * @returns {JSX.Element} Atomic navigation menu item.
  */
 export default function ItemNav (props) {
+    const handleClick = () => {
+        if (props.onClick) {
+            props.onClick();
+        }
+    }
+
     return (
-        <a className='flex flex-row items-center min-w-2 text-center px-14 py-3 bg-pink text-2xl border-4 border-black shadow-2xl font-semibold text-white ' href={`#${props.navigateTo}`}>
+        <a 
+            className={props.selected ? 'nav__item nav__item-selected' : 'nav__item'}
+            href={`#${props.navigateTo}`}
+            onClick={handleClick}
+        >
             {props.innerText.toUpperCase()}
         </a>
     )
--- app/components/nav.jsx
+++ app/components/nav.jsx
@@ -1,15 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 import ItemNav from "../atomic/item-nav";
 
 export default function Nav () {
+    const [inicio, setInicio] = useState(true);
+    const [cronograma, setCronograma] = useState(false);
+    const [patrocinadores, setPatrocinadores] = useState(false);
+    const [quemSomos, setQuemSomos] = useState(false);
+    const [faq, setFaq] = useState(false);
+    const [comoChegar, setComoChegar] = useState(false);
+
+    const handleClickInicio = () => {
+        setInicio(true);
+        setCronograma(false);
+        setPatrocinadores(false);
+        setQuemSomos(false);
+        setFaq(false);
+        setComoChegar(false);
+    }
+
+    const handleClickCronograma = () => {
+        setInicio(false);
+        setCronograma(true);
+        setPatrocinadores(false);
+        setQuemSomos(false);
+        setFaq(false);
+        setComoChegar(false);
+    }
+
+    const handleClickPatrocinadores = () => {
+        setInicio(false);
+        setCronograma(false);
+        setPatrocinadores(true);
+        setQuemSomos(false);
+        setFaq(false);
+        setComoChegar(false);
+    }
+
+    const handleClickQuemSomos = () => {
+        setInicio(false);
+        setCronograma(false);
+        setPatrocinadores(false);
+        setQuemSomos(true);
+        setFaq(false);
+        setComoChegar(false);
+    }
+
+    const handleClickFaq = () => {
+        setInicio(false);
+        setCronograma(false);
+        setPatrocinadores(false);
+        setQuemSomos(false);
+        setFaq(true);
+        setComoChegar(false);
+    }
+
+    const handleClickComoChegar = () => {
+        setInicio(false);
+        setCronograma(false);
+        setPatrocinadores(false);
+        setQuemSomos(false);
+        setFaq(false);
+        setComoChegar(true);
+    }
+
     return (
-        <div className="mx-4 my-3">
-            <ItemNav navigateTo="inicio" innerText="início" />
-            <ItemNav navigateTo="cronograma" innerText="cronograma" />
-            <ItemNav navigateTo="patrocinadores" innerText="Patrocinadores" />
-            <ItemNav navigateTo="quem-somos" innerText="quem Somos?" />
-            <ItemNav navigateTo="faq" innerText="FAQ" />
-            <ItemNav navigateTo="como-chegar" innerText="Como Chegar" />
+        <div className="nav">
+            <ItemNav navigateTo="inicio" innerText="início" selected={inicio} onClick={handleClickInicio} />
+            <ItemNav navigateTo="cronograma" innerText="cronograma" selected={cronograma} onClick={handleClickCronograma} />
+            <ItemNav navigateTo="patrocinadores" innerText="Patrocinadores" selected={patrocinadores} onClick={handleClickPatrocinadores} />
+            <ItemNav navigateTo="quem-somos" innerText="Quem Somos?" selected={quemSomos} onClick={handleClickQuemSomos} />
+            <ItemNav navigateTo="faq" innerText="FAQ" selected={faq} onClick={handleClickFaq} />
+            <ItemNav navigateTo="como-chegar" innerText="Como Chegar?" selected={comoChegar} onClick={handleClickComoChegar} />
         </div>
     )
 }
