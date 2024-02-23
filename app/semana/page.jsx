b461db52f326ad86c3fe831e5abfbca432bf0dba
Od4ir <od4ircode@gmail.com>
2024-02-23 00:50:42 -0300

    patrocinadores' layout added

--- app/semana/page.jsx
+++ app/semana/page.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Nav from "../components/nav";
 import Presentation from "../components/presentation";
-
+import Patrocinadores from "../components/patrocinadores";  
 export default function Semana() {
     return (
         <div>
             <Nav />
             <Presentation />
+            <Patrocinadores />
         </div>
     )
 }
