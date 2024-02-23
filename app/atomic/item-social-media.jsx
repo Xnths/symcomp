18240df20157920d1dc44e61e615827aa099feab
Od4ir <od4ircode@gmail.com>
2024-02-22 22:27:42 -0300

    presentation done

--- app/atomic/item-social-media.jsx
+++ app/atomic/item-social-media.jsx
@@ -11,24 +11,24 @@ export default function ItemSocialMedia(props) {
         <div className="presentation__socialmedia">
             <p>REDES SOCIAIS</p>
             <div className="presentation__socialmedia_icons">
-                <div>
+                <a href="https://www.instagram.com/symcomp.imeusp?igsh=MTYybmM0eTBicjh3cQ==" target="_blank">
                     <div>
                         <ItemInstaIcon fillColor="#FF006E"/>
                     </div>
                     <p>Instagram</p>
-                </div>
-                <div>
+                </a >
+                <a href="mailto:semanadacomputacao@ime.usp.br" target="_blank">
                     <div>
                         <ItemIconEmail fillColor="#FF006E"/>
                     </div>
                     <p>E-mail</p>
-                </div>
-                <div>
+                </a>
+                <a href="https://www.linkedin.com/in/semana-da-computa%C3%A7%C3%A3o-ime-usp-00420828a/" target="_blank">
                     <div>
                         <ItemEventLinkedin fillColor="#FF006E"/>
                     </div>
                     <p>LinkedIn</p>
-                </div>
+                </a>
             </div>
         </div>
     )
