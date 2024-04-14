7d2ed7873d9b80332ac57cec8e7b8c82c52de2c0
Od4ir <od4ircode@gmail.com>
2024-04-14 01:33:01 -0300

    feat: Adding new component to footer, update in symcomp's image in footer and insertion of GDR's logotype

--- app/components/oferecimento.jsx
+++ app/components/oferecimento.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import SymcompSignature from "../atomic/symcomp-signature";
+import FooterOferecimentoBox from "../atomic/footer-oferecimento-box";
 
 export default function Oferecimento () {
     return (
         <div className="oferecimento__container">
-            <div className="oferecimento__title">Oferecimento</div>
-            <SymcompSignature />
+            <FooterOferecimentoBox ImgPath="/logos/logo_semana_horizontal_rosa.png" ImgAlt="Logotipo horizontal SymComp - Simpósio de Computação" TitleText="OFERECIMENTO"/>
+
+            <FooterOferecimentoBox ImgLink="https://gdg.community.dev/gdg-sao-paulo/" ImgPath="/logos/logo_google_developer_groups.png" ImgAlt="Logotipo horizontal SymComp - Simpósio de Computação" TitleText="QUEM ACREDITA"/>
         </div>
     )
 }
