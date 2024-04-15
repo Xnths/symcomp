c323df7075f6c0dbd4bc17e78b19b15e45a1afd1
Xnths <jonathascastilhoes@gmail.com>
2024-04-15 12:59:49 -0300

    rev: review code writting

--- app/atomic/new-schedule-box.jsx
+++ app/atomic/new-schedule-box.jsx
@@ -1,37 +1,56 @@
-"use client"
-import React from 'react';
+"use client";
+import React from "react";
 import DownArrow from "../components/svgs/downArrow";
-import { useState } from "react"
-
+import { useState } from "react";
 
 const NewScheduleBox = (props) => {
-    const [ isTopicOpen, setIsTopicOpen ] = useState(false);
-    
-    const openTopic = () => {
-        setIsTopicOpen(!isTopicOpen)
-    }
-    return (
-        <div id='newschedule__container'>
-            <div className='newschedule__schedulebox'>
-                {props.ImgPath && <img src={`palestrantes/${props.ImgPath}`} alt={`Foto de´ ${props.lecturer}`} />}
-                <div id='newschedule__fulltitle'>
-                    <div id="newschedule__title">{props.title}</div>
-                    <div id='newschedule__lecturer'>{props.lecturer}</div>
-                </div>
-                <div className='newschedule__buttons' id='newschedule__saibamais' onClick={openTopic}>
-                    <p>SAIBA MAIS</p>
-                    <DownArrow className={`flex transition duration-500 ease-in-out ${isTopicOpen ? 'rotate-180' : ''} pl-left-2`} fill={isTopicOpen ? '#fff' : '#fff'}/>
-                </div>
-                <div className='newschedule__buttons' id='newschedule__time'>{props.time}</div>
-            </div>
-                {isTopicOpen && 
-                    <div className="newschedule__schedulebox" id='newschedule__about'>
-                        <div><p>Descrição Palestra:</p> {props.description}</div>
-                        <div><p>Descrição Palestrante:</p> {props.aboutLecturer}</div>
-                    </div>
-                }
+  const [isTopicOpen, setIsTopicOpen] = useState(false);
+
+  const openTopic = () => {
+    setIsTopicOpen(!isTopicOpen);
+  };
+  return (
+    <div id="newschedule__container">
+      <div className="newschedule__schedulebox">
+        {props.ImgPath && (
+          <img
+            src={`palestrantes/${props.ImgPath}`}
+            alt={`Foto de´ ${props.lecturer}`}
+          />
+        )}
+        <div id="newschedule__fulltitle">
+          <div id="newschedule__title">{props.title}</div>
+          <div id="newschedule__lecturer">{props.lecturer}</div>
         </div>
-    );
+        <div
+          className="newschedule__buttons"
+          id="newschedule__saibamais"
+          onClick={openTopic}
+        >
+          <p>SAIBA MAIS</p>
+          <DownArrow
+            className={`flex transition duration-500 ease-in-out ${
+              isTopicOpen ? "rotate-180" : ""
+            } pl-2`}
+            fill={isTopicOpen ? "#fff" : "#fff"}
+          />
+        </div>
+        <div className="newschedule__buttons" id="newschedule__time">
+          {props.time}
+        </div>
+      </div>
+      {isTopicOpen && (
+        <div className="newschedule__schedulebox" id="newschedule__about">
+          <div>
+            <p>Descrição Palestra:</p> {props.description}
+          </div>
+          <div>
+            <p>Descrição Palestrante:</p> {props.aboutLecturer}
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default NewScheduleBox;
--- app/components/schedule-nav.jsx
+++ app/components/schedule-nav.jsx
@@ -2,67 +2,71 @@ import React, { useState } from "react";
 import ItemNav from "../atomic/item-nav";
 import ScheduleNavItem from "../atomic/schedule-nav-item";
 import ScheduleDetailsItem from "../atomic/schedule-details-item";
-import { CgCoffee } from "react-icons/cg"
+import { CgCoffee } from "react-icons/cg";
 import NewScheduleBox from "../atomic/new-schedule-box";
 
-export default function ScheduleNav ({ sheetdata }) {
-    const [activeDay, setActiveDay] = useState('SEG');
+export default function ScheduleNav({ sheetdata }) {
+  const [activeDay, setActiveDay] = useState("SEG");
 
-    const handleClick = (day) => {
-        setActiveDay(day);
-    }
+  const handleClick = (day) => {
+    setActiveDay(day);
+  };
 
-    const days = [
-        { date: "22/04", dayWeek: "SEG" },
-        { date: "23/04", dayWeek: "TER" },
-        { date: "24/04", dayWeek: "QUA" },
-        { date: "25/04", dayWeek: "QUI" },
-        { date: "26/04", dayWeek: "SEX" },
-    ];
+  const days = [
+    { date: "22/04", dayWeek: "SEG" },
+    { date: "23/04", dayWeek: "TER" },
+    { date: "24/04", dayWeek: "QUA" },
+    { date: "25/04", dayWeek: "QUI" },
+    { date: "26/04", dayWeek: "SEX" },
+  ];
 
-    return (
-        <div className="schedule__container">
-            <h1 className="schedule__title"><p className="titles">CRONOGRAMA</p></h1>
-            <div className="schedule">
-                {days.map((day) => (
-                    <ScheduleNavItem 
-                        key={day.date} 
-                        date={day.date} 
-                        dayWeek={day.dayWeek} 
-                        onClick={() => handleClick(day.dayWeek)} 
-                        selected={activeDay === day.dayWeek} 
-                    />
-                ))}
-            </div>
-            <div>
-                {sheetdata &&
-                    sheetdata.map((item, index) => {
-                        if (item[0] === activeDay && item[2] != "-") {
-                            return (
-                                // Carrega as palestras do dia!
-                                <div>
-                                    <NewScheduleBox
-                                        key={index}
-                                        time={item[1]}
-                                        theme="Palestra"
-                                        description={item[3]}
-                                        title={item[2]}
-                                        lecturer={item[4]}
-                                        aboutLecturer={item[5]}
-                                        ImgPath={item[6]}
-                                    />
-                                </div>
-                            )
-                        }
-                    })
-                }
-                <div className="flex flex-col items-center w-screen px-4 ">
-                    <div className="flex flex-row items-center justify-center space-x-4 lg:w-[600px] w-full border-4 border__shadow px-8 py-4 text-left bg-primary">
-                        <CgCoffee size={64} className="text-white" />
-                        <span className="text-6xl font-bold text-center text-white">COFFEE BREAK</span>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="schedule__container pb-12">
+      <h1 className="schedule__title">
+        <p className="titles">CRONOGRAMA</p>
+      </h1>
+      <div className="overflow-x-auto lg:w-auto w-full px-4">
+        <div className="w-2/5 flex flex-row">
+          {days.map((day) => (
+            <ScheduleNavItem
+              key={day.date}
+              date={day.date}
+              dayWeek={day.dayWeek}
+              onClick={() => handleClick(day.dayWeek)}
+              selected={activeDay === day.dayWeek}
+            />
+          ))}
         </div>
-    )
+      </div>
+      <div className="w-full flex flex-col items-center justify-center">
+        {sheetdata &&
+          sheetdata.map((item, index) => {
+            if (item[0] === activeDay && item[2] != "-") {
+              return (
+                <div>
+                  <NewScheduleBox
+                    key={index}
+                    time={item[1]}
+                    theme="Palestra"
+                    description={item[3]}
+                    title={item[2]}
+                    lecturer={item[4]}
+                    aboutLecturer={item[5]}
+                    ImgPath={item[6]}
+                  />
+                </div>
+              );
+            }
+          })}
+        <div className="flex flex-col items-center w-screen px-4 ">
+          <div className="flex flex-row items-center justify-center space-x-4 lg:w-[600px] w-full border-4 border__shadow px-8 py-4 text-left bg-primary">
+            <CgCoffee size={64} className="text-white" />
+            <span className="text-6xl font-bold text-center text-white">
+              COFFEE BREAK
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
