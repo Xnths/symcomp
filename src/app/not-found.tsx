1b38f695732d261daab651962a3af60250eea851
Xnths <jonathascastilhoes@gmail.com>
2024-08-30 14:33:54 -0300

    feat: adding responsivity to not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,10 +3,10 @@ export default function NotFoundPage () {
         <div className="flex flex-row items-center justify-center h-full w-full">
             <div className="flex w-4/5 h-screen flex-col items-center justify-center gap-2">
                     <div className="w-full flex flex-row justify-start">
-                        <h2 className="font-extrabold bg-primary border-4 px-8 py-4 border-black shadow-solid w-fit text-8xl">QUE EVENTO É ESSE?</h2>
+                        <h2 className="font-extrabold bg-primary border-4 px-8 py-4 border-black shadow-solid w-fit text-2xl sm:text-8xl">QUE EVENTO É ESSE?</h2>
                     </div>
-                    <div className="w-full flex flex-row justify-end">
-                        <p className="font-extrabold bg-white border-4 px-8 py-4 border-black shadow-solid w-fit text-2xl">VISH, PARECE QUE ESSA PÁGINA NÃO EXISTE <span className="opacity-45">(AINDA?)</span></p>
+                    <div className="sm:w-full flex flex-row justify-end">
+                        <p className="font-extrabold bg-white border-4 px-8 py-4 border-black shadow-solid w-fit text-xm sm:text-2xl">VISH, PARECE QUE ESSA PÁGINA NÃO EXISTE <span className="opacity-45">(AINDA?)</span></p>
                     </div>
             </div>
         </div>
