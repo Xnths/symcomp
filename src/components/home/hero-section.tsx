7c7eee15171c645ee4cd968fa8b5230da712e572
Xnths <jonathascastilhoes@gmail.com>
2024-08-30 14:20:45 -0300

    feat: adding responsivity to heero section

--- src/components/home/hero-section.tsx
+++ src/components/home/hero-section.tsx
@@ -17,16 +17,16 @@ export const HeroSection = () => {
     ]
 
     return (
-        <div id="home" className="px-4 bg-background h-[calc(100vh-100px)]">
+        <div id="home" className="bg-background h-[calc(100vh-100px)]">
             <div className="flex flex-col justify-between items-center h-full py-[100px]">
-                <div className="flex flex-col gap-4 w-2/3">
-                    <h1 className="font-extrabold text-black text-4xl px-8 py-2 bg-secondary shadow-solid w-fit border-4 border-black">SIMPÓSIO DA COMPUTAÇÃO</h1>
-                    <p className="text-white text-2xl w-2/5">Organizando eventos sobre computação, aproximando a comunidade <strong>interna</strong> e <strong>externa</strong> do curso de Bacharelado em Ciência da Computação da USP Butantã.</p>
+                <div className="px-4 flex flex-col gap-4 sm:w-2/3">
+                    <h1 className="font-extrabold text-black text-xl sm:text-4xl px-4 sm:px-8 py-2 bg-secondary shadow-solid w-fit border-4 border-black">SIMPÓSIO DA COMPUTAÇÃO</h1>
+                    <p className="text-white text-lg sm:text-2xl sm:w-2/5 hyphens-auto ">Organizando eventos sobre computação, aproximando a comunidade <strong>interna</strong> e <strong>externa</strong> do curso de Bacharelado em Ciência da Computação da USP Butantã.</p>
                 </div>
-                <div className="flex flex-row w-full justify-center gap-10">
+                <div className="flex flex-row w-full justify-center gap-4 sm:gap-10 overflow-auto">
                     {highlights.map(highlight => (
-                        <div key={highlight.label} className="flex flex-col justify-center items-center gap-4 border-4 border-black shadow-solid bg-secondary p-10">
-                            <span className="text-black font-extrabold">
+                        <div key={highlight.label} className="flex flex-col justify-center items-center gap-4 border-4 border-black shadow-solid bg-secondary p-4 sm:p-10">
+                            <span className="text-black font-extrabold text-xs sm:text-lg text-center">
                                 {highlight.label.toUpperCase()}
                             </span>
                                 {highlight.icon}
