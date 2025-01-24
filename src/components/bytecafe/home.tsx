251339aeed7f622deb9ebc835b66fd444947fc20
Od4ir <od4ir@usp.br>
2025-01-24 11:32:56 -0300

    refact | Refactoring home component

--- src/components/bytecafe/home.tsx
+++ src/components/bytecafe/home.tsx
@@ -1,42 +1,50 @@
 export const BCHome = () => {
     return (
         <div className="flex flex-col justify-center items-center bg-bc-white text-center lg:text-left lg:min-w-[65rem] font-mono lg:items-start lg:px-[2rem] lg:h-[90vh] lg:justify-center">
-            <div className="flex flex-col justify-center items-center md:flex-col-reverse lg:flex-row lg:flex-row-reverse lg:justify-between lg:items-center lg:w-full">
-                <Logo />
-                <Title />
-            </div>
-            <div className="hidden sm:block text-bc-brown font-mono font-bold text-[1.1rem] w-[20rem] pb-4">
-                <p>VENHA CONHECER O CURSO DE COMPUTAÇÃO DA USP!</p>
-            </div>
+            <Header />
+            <Invitation />
             <Description />
-            <ParticipeButton/>
+            <ParticipeButton />
         </div>
-    )
-}
+    );
+};
+
+const Header = () => (
+    <div className="flex flex-col justify-center items-center md:flex-col-reverse lg:flex-row lg:flex-row-reverse lg:justify-between lg:items-center lg:w-full lg:h-[15rem]">
+        <Logo />
+        <Title />
+    </div>
+);
 
 const Logo = () => (
     <div className="relative">
         <img
             alt="Logo do ByteCafé - Uma xícara feita de chaves (operador matemático) com números em binário (0 e 1) representando a bebida em cor de caramelo. Os números em binário representam o código do curso. Há um vapor, também em cor de caramelo saindo da xícara."
-            src="/logo/logo_bytecafe.svg" className="md:h-[16rem] mt-4 md:mt-0 lg:h-[20rem]"
+            src="/logo/logo_bytecafe.svg"
+            className="md:h-[16rem] mt-4 md:mt-0 lg:h-[20rem] lg:h-[24rem]"
         />
         <div className="hidden lg:block">
             <img
-            alt="Reflexo do logo do ByteCafé"
-            src="/logo/logo_bytecafe.svg" className="mt-0 lg:h-[20rem] absolute top-full left-0 transform rotate-180 opacity-20"
-            style={{ clipPath: 'inset(0 0 0 0)', maskImage: 'linear-gradient(to top, rgba(0,0,0,0.8), rgba(0,0,0,0))' }}
+                alt="Reflexo do logo do ByteCafé"
+                src="/logo/logo_bytecafe.svg"
+                className="mt-0 lg:h-[24rem] absolute top-full left-0 transform rotate-180 opacity-20"
+                style={{ clipPath: 'inset(0 0 0 0)', maskImage: 'linear-gradient(to top, rgba(0,0,0,0.8), rgba(0,0,0,0))' }}
             />
         </div>
     </div>
 );
-
-
 
 const Title = () => (
     <span className="text-bc-caramel">
         <p className="text-[1.5rem] h-[1.5rem] md:text-[5rem] md:h-[5rem]">UM PROJETO</p>
         <h1 className="font-thin text-[2.5rem] md:text-[7rem] md:font-bold">SYMCOMP</h1>
     </span>
+);
+
+const Invitation = () => (
+    <div className="hidden sm:block text-bc-brown font-mono font-bold text-[1.1rem] w-[20rem] pb-4">
+        <p>VENHA CONHECER O CURSO DE COMPUTAÇÃO DA USP!</p>
+    </div>
 );
 
 const Description = () => (
